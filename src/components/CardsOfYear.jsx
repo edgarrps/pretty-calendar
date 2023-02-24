e09e69f8f29acf457c27cd466381bcae3733ca23
Edgar Santos <edgarrps@gmail.com>
2023-02-24 15:04:10 -0400

    Initial implementation of save checked date

--- src/components/CardsOfYear.jsx
+++ src/components/CardsOfYear.jsx
@@ -22,7 +22,7 @@ export const CardsOfYear = (props) => {
                 <hr className='pb-1 bg-purple-100'/>
                 <div className='grid grid-cols-7 pt-2 pb-3 bg-pink-50 font-bold text-center'>{daysOfTheWeek.map(position => (<div key={position}>{position}</div>))}</div>
                 {weeks.map((week, key) => (<div key={key} className='grid bg-gray-50 grid-cols-7 text-green-700 grid-cols-end-2 p-2 place-items-center text-center'>
-                    {week.map(day => (<Days key={day._d.getTime() + props.month} day={day} month={props.month} year={props.currentYear} />))}
+                    {week.map(day => (<Days key={day._d.getTime() + props.month} day={day} month={props.month} year={props.currentYear} checkDate={props.checkDate} setCheckDate={props.setCheckDate} />))}
                 </div>
 
                 ))
