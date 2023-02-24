72d89ae562ef14ac38775d7fe347e10409700470
Edgar Santos <edgarrps@gmail.com>
2023-02-23 22:45:43 -0400

    Added condicional dates and onClick event for available dates

--- src/components/CardsOfDays.jsx
+++ src/components/CardsOfDays.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const CardsOfDays = (props) => {
   const [dayBtn, setDayBtn] = useState('')
+  const day = props.day._d
+
+  useEffect(() => {
+    const currentMonth = new Date(props.month + ',01,' + props.year)
+
+    day.getMonth() !== currentMonth.getMonth() ? setDayBtn('noInMonth') : ''
+  })
 
   const handleClickDate = () => {
-    ((dayBtn === '') ? setDayBtn('select') : ('select' ? setDayBtn('') : ''))
-    }
-  
+    (dayBtn == '') ? setDayBtn('ok') : setDayBtn('')
+  }
+
 
   return (
-    <div onClick={handleClickDate} className='font-semibold'>{props.day.format('DD').toString()}</div>
+    <div onClick={handleClickDate} className={`cursor-pointer select-none	font-semibold w-[27px] h-[27px] rounded-lg ${dayBtn == 'ok' ? 'bg-yellow-200' : ''} ${dayBtn === 'noInMonth' ? 'text-gray-300 cursor-not-allowed' : ''}`}> {props.day.format('DD').toString()}</div>
   )
 }
 
